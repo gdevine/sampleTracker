e2a85ab05fc46a99f1eb0f05160e616ad7c29d7b
gerarddevine <g.devine@uws.edu.au>
2014-02-13 11:47:24 +1100

    Refactor static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,17 @@ describe "Static pages" do
       expect(page).to have_content('HIE Sample Tracker')
     end
     
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("HIE Sample Tracker | Home")
+      expect(page).to have_title("HIE Sample Tracker")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+    
+    
   end
   
   describe "Help page" do
