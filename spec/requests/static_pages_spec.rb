8dd5162845f5ff3e7abde87ffd5fee0cd3182358
gerarddevine <g.devine@uws.edu.au>
2014-02-13 16:23:10 +1100

    Add User

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,51 +1,37 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'HIE Sample Tracker'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('HIE Sample Tracker')
-    end
-    
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("HIE Sample Tracker")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
-    
-    
+    it { should have_content('HIE Sample Tracker') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
   
-  describe "Help page" do
-
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
     
-    it "should have the right title" do
-      visit '/static_pages/help'
-      expect(page).to have_title("HIE Sample Tracker | Help")
-    end
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
   
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About the HIE Sample Tracker'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About the HIE Sample Tracker')
-    end
-    
-    it "should have the right title" do
-      visit '/static_pages/about'
-      expect(page).to have_title("HIE Sample Tracker | About")
-    end
+    it { should have_content('About the HIE Sample Tracker') }
+    it { should have_title(full_title('About')) }
+  end
+  
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
   
 end
